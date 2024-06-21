75f3b3646f81f3e19385bf7e216fcb2034d60182
Ayush Shaurya Jha <164651751+jhaayush2004@users.noreply.github.com>
2024-06-21 18:23:43 +0530

    Update localama.py

--- chatbot/localama.py
+++ chatbot/localama.py
@@ -21,11 +21,11 @@ prompt=ChatPromptTemplate.from_messages(
 )
 ## streamlit framework
 
-st.title('Langchain Demo With LLAMA2 API')
+st.title('Langchain Demo With GEMMA:2b API')
 input_text=st.text_input("Search the topic u want")
 
-# ollama LLAma2 LLm 
-llm=Ollama(model="llama2")
+# ollama Gemma:2b LLm 
+llm=Ollama(model="Gemma")
 output_parser=StrOutputParser()
 chain=prompt|llm|output_parser
 
